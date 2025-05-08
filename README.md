7f77213da238dfdcdee0084db8ebb6e2a6b5da23
Abhinav Mehrotra <amehrotra@Abhinavs-MacBook-Air.local>
2025-05-08 20:38:55 +0100

    Added readme and support for multiple images

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# t2i_demo
+## Demo app for Text2Image models
+
+This repository supports displaying the images generatde by multiple text-to-image foundation models.
+
+## Requirements
+
+```
+git clone git@github.com:AbhinavMehrotra/t2i_demo.git
+cd t2i_demo
+pip install -r requirements.txt
+``` 
+
+## Run the app on a browser
+
+```
+streamlit run app.py
+```
+
+Now, you can access the app on your browser with http://localhost:8501/
--- app.py
+++ app.py
@@ -7,8 +7,8 @@ import numpy as np
 
 # --- Function to interact with the image generation model ---
 # This is a placeholder. You'll need to replace this with your actual logic
-def generate_image_from_text(prompt):
-    time.sleep(3)
+def generate_image_from_text(prompt, model):
+    time.sleep(1)
     # Dummy image (replace this with your actual image generation model output)
     img = Image.new("RGB", (512, 512), color="lightblue")
     return img
@@ -22,23 +22,41 @@ st.title("Demo: LVM Foundation Model")
 
 # Input
 prompt = st.text_area("Enter text to generate image:", "")
+generate_btn = st.button("Generate Image")
+
+# Processing and status update fields
+processing_spinner = st.empty()
+status_update = st.empty()
 
 # Image display placeholder
-image_slot = st.empty()
+col1, col2 = st.columns([1, 1])
+with col1:
+    image_slot_flux_12b = st.empty()
+    image_slot_sd35 = st.empty()
+with col2:
+    image_slot_flux_5b = st.empty()
+    image_slot_sd35_edit = st.empty()
 
 # Show gray placeholder initially
 placeholder_image = gray_placeholder_image()
-image_slot.image(placeholder_image, caption="", width=250)
+image_slot_flux_12b.image(placeholder_image, caption="Flux 12B", width=250)
+image_slot_flux_5b.image(placeholder_image, caption="Flux 5B", width=250)
+image_slot_sd35.image(placeholder_image, caption="SD 3.5M", width=250)
+image_slot_sd35_edit.image(placeholder_image, caption="SD 3.5M EDiT", width=250)
 
 
-if st.button("Generate Image"):
+if generate_btn:
     if prompt:
         # Display a processing message
         with st.spinner("Generating image..."):
+            image_slot_flux_12b.image(placeholder_image, caption="", width=250)
+            image_slot_flux_5b.image(placeholder_image, caption="", width=250)
             try:
-                generated_image = generate_image_from_text(prompt)
-                st.success("Image generated successfully!")
-                image_slot.image(generated_image, caption=f"Generated Image for: '{prompt}'", width=250)
+                generated_image_flux_12b = generate_image_from_text(prompt, "flux_12b")
+                generated_image_flux_5b = generate_image_from_text(prompt, "flux_5b")
+                status_update.success("Image generated successfully!")
+                image_slot_flux_12b.image(generated_image_flux_12b, caption=f"Flux 12B", width=250)
+                image_slot_flux_5b.image(generated_image_flux_5b, caption=f"Flux 5B", width=250)
             except requests.exceptions.RequestException as e:
                 st.error(f"Error fetching placeholder image: {e}")
             except Exception as e:
